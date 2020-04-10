27877f872eca9507affbf460010fbc1728a7a1b8
Aqdas <aqdasak@gmail.com>
2020-04-10 08:48:03 +0530

    escape sequence in function call

--- Healthy pogrammer.py
+++ Healthy pogrammer.py
@@ -1,5 +1,5 @@
 from pygame import mixer
-from time import time
+from time import time, sleep
 from datetime import datetime
 
 
@@ -11,18 +11,23 @@ def play_music(file, stopper):
         a = input()
         if a == stopper:
             mixer.music.stop()
+            print("\n::::::::: GREAT :::::::::")
+            sleep(2)
+            cls()
+            print(":: :: :: HEALTHY PROGRAMMER :: :: ::")
             break
+
 
 
 def log(msg):
     with open("Healthy Programmer.txt", "a") as f:
-        f.write(f"{msg} : {datetime.now()}\n")
+        f.write(f"{msg}\t: {datetime.now()}\n")
 
 
 def cls():
-    import os
+    from os import system, name
     # for windows os.name is 'nt'
-    os.system('cls' if os.name == 'nt' else 'clear')
+    system('cls' if name == 'nt' else 'clear')
 
 
 if __name__ == '__main__':
@@ -33,6 +38,7 @@ if __name__ == '__main__':
     waterSec = 25*60
     eyesSec = 20*60
     exerSec = 45*60
+    print(":: :: :: HEALTHY PROGRAMMER :: :: ::")
     while True:
         if (time() - initWater) > waterSec:
             print("Time to drink water. Input drank to stop music")
